e099c1ec813e5f7ad73acde2eacbfb2d480ed83a
Sophia Davis <scdgrapefruit@gmail.com>
2014-07-21 17:20:35 -0400

    Refactor and add some print statements

--- httpServer.py
+++ httpServer.py
@@ -2,8 +2,10 @@ import socket
 import sys
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create server socket
-    s.bind(('localhost', 8000)) # bind to port 8000, on this machine
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create server socket (IPv4, stream = TCP protocol)
+    
+    host = 'localhost'
+    port = 8000
     try:
         s.bind((host, port)) # bind to host 'localhost', port 8000, on this machine
         print "Socket bind complete -- host " + host + ", port: " + str(port)
@@ -13,13 +15,23 @@ def main():
         sys.exit(1)
         
     s.listen(5) # up to 5 connect requests (waiting to connect)
+    print "Listening..."
     
     while 1:
         client, address = s.accept() # client = new socket object to communicate with connection
+        print "Connected to " + address[0] + ":" + str(address[1])
+        
         data = client.recv(1024) # arg is max num bytes to receive
-        if data:
-            client.send(data) # send data back to client
-        client.close()
+        
+        reply = 'OK...' + data
+        
+        if not data:
+            break
+        print "Got data: " + data
+        client.sendall(reply) # send data back to client (sendall sends all available data, unlike send)
+        
+    client.close()
+    s.close()
 
 if __name__ == "__main__":
 	main()
